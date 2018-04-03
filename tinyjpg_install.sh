89f2249c85cf05af7a2ee5dd3025bcbbf4c69690
O.J <kristian.lisov@gmail.com>
2018-04-03 10:42:21 +0300

    Update tinyjpg_install.sh
    update install

--- tinyjpg_install.sh
+++ tinyjpg_install.sh
@@ -27,10 +27,9 @@ mkdir -p $CONF_DIR $LOG_DIR
 echo -n "Fetching TinyJPG from $URL: "
 $download_cmd || die "Error when downloading TinyJPG from $URL"
 $conf_download_cmd || die "Error when downloading config file TinyJPG from $CONF_URL"
-echo "Install TinyJPG: done"
+echo -e "Install TinyJPG: \x1B[32m done \x1B[0m"
 
 echo -n "Set permission execute TinyJPG: "
 $PERM || die "Error permission execut TinyJPG from $TARGET_DIR"
-echo "done"
-
-echo "Finished"
+echo -e "\x1B[32m done \x1B[0m"
+echo -e "\x1B[32m Finished \x1B[0m"
