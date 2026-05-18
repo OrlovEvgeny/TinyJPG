5a9936f721d02b6de68d7bbf03e4627a32630bee
Evgenii Orlov <evgenii@eorlov.org>
2026-05-18 13:32:18 +0200

    chore: align source formatting

--- src/app/main.cc
+++ src/app/main.cc
@@ -1,8 +1,9 @@
-#include "tinyjpg/app/cli.hh"
-
 #include <span>
 
+#include "tinyjpg/app/cli.hh"
+
 int main(int argc, char* argv[]) {
-  const auto args = std::span<char const* const>{argv, static_cast<std::size_t>(argc)};
+  const auto args =
+      std::span<char const* const>{argv, static_cast<std::size_t>(argc)};
   return static_cast<int>(tinyjpg::app::run(args));
 }
